b3a73592ac2d843ee745381b3e18f2cc123e45f1
Mohamed Galal <mohamed@codescalers.com>
2017-01-02 16:23:00 +0200

    check if docker container exists and running

--- base_docker/install_base_docker.sh
+++ base_docker/install_base_docker.sh
@@ -20,10 +20,24 @@ function docker_ip ()
   if [[ $@ -eq 1 ]]; then
     doc_name=$1
     doc_ip=$(docker inspect ${doc_name} | grep IPAdd | grep -v -m1 'null,$' | awk -F : '{print $2}' | sed -E 's/"|\ |,//g') &> /dev/null | { echo " [Error] Can't find this container ${doc_name} && exit 1;"
+    echo ${doc_ip}
   else
     echo "[Error] Function docker_ip get only 1 arg => docker name"
+    exit 1
   fi
+}
 
+function container_running_check()     # check if docker container exist and running, ex: pxeboot and jumpscale7 containers
+{
+  [[ ${#@} -eq 1 ]] || {echo "[Error] Only one arg (container name) provide for func container_running_check"; exit 1;}
+  container=$1
+  [[ $(docker ps -a | grep ${container}) ]] || {echo "[Error] Can't find this container :( "; exit 1;}
+  status=$(docker ps -a --format "{{.Names}}\t{{.Status}}" | grep ${container} | awk '{print $2}')
+  if [[ ${status} == "Up" ]]; then
+    return 0
+  else
+    return 1
+  fi
 }
 
 function remove_ovc_dockers ()
@@ -50,9 +64,9 @@ function remove_images ()
     images=$(${docpath} images | grep -E '^openvcloud/' | awk '{print $3}')
     echo ${images} | xargs -n 1 ${docpath} rmi -f               #modify at first run
   fi
-  while [[ $(${docpath} images | grep -i 'none') ]]
+  while [[ $(${docpath} images | grep -i 'none') ]]             #because of alot of none images generated
   do
     images=$(${docpath} images | grep -i 'none' | awk '{print $3}')
-    echo ${images} | xargs -n 1 ${docpath} rmi -f                #modify at first run
+    echo ${images} | xargs -n 1 ${docpath} rmi -f               #modify at first run
   done
 }
