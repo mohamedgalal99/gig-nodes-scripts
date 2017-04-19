96a755fe4dd634b3b0b2c72d543d848e0da1504c
Mohamed Galal <mohamed@codescalers.com>
2017-04-19 12:33:19 +0200

    add some comments to know steps

--- clean_network_space.sh
+++ clean_network_space.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+#############################
+# This SCript used to clean #
+# unused penVswitch bridges #
+#############################
+
 export LC_ALL=${LANG}
 declare -a used_network
 
-for i in $(virsh list --all --name)
+for i in $(virsh list --all --name)	#get all vms on node
 do
-  network=$(virsh dumpxml ${i} | grep -E "<source network='space_....'" | awk -F"'" '{print $2}')
+  network=$(virsh dumpxml ${i} | grep -E "<source network='space_....'" | awk -F"'" '{print $2}')  #which ovs bridge vm attached to
   used_network+=(${network})
   #echo ${network}
 done
 
-for i in $(ovs-vsctl list-br | grep -E "^space_....")
+for i in $(ovs-vsctl list-br | grep -E "^space_....") # get all space bridges
 do
-  if [[ ! ${used_network[@]} =~ ${i} ]]; then
+  if [[ ! ${used_network[@]} =~ ${i} ]]; then # check this bridge not in use
     echo ${i}
-    for port in $(ovs-vsctl list-ports ${i})
+    for port in $(ovs-vsctl list-ports ${i}) # get ports to delete (not required)
     do
       ovs-vsctl del-port ${i} ${port} && echo "[+] Port ${i} deleted" || echo "[-] Can't delete Port ${i}"
     done
