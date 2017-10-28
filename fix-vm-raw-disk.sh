f93ae619b24710f346155e9c3fd193fa2377542a
Mohamed Galal <mohamed@codescalers.com>
2017-10-28 20:39:13 +0200

    fsck for vm disk

---
+++ fix-vm-raw-disk.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+[[ $# != 1 ]] && { echo "[*] This function take vm name ex: vm-123"; exit 1; }
+vm="$1"
+
+[[ -d "/mnt/vmstor/${vm}" ]] || { echo "Can't find dir for ${vm}"; exit 1; }
+cd /mnt/vmstor/${vm}
+[[ -f "bootdisk-${vm}.raw" ]] || { echo "Can't find boot disk for vm ${vm}"; exit 1; }
+
+loop=$(losetup -f)
+echo "[*] will use ${loop}"
+losetup -P ${loop} bootdisk-${vm}.raw  #mount disk in loop with its pations
+fdisk -l ${loop} | grep "Linux filesystem"
+if [[ $? == 0 ]]; then
+	device=$(fdisk -l /dev/loop0 | grep "Linux filesystem" | awk '{print $1}')
+	echo "[+] Start check on device ${device}"
+	fsck ${device}
+	echo "[+] Removing loop device"
+	losetup -d ${loop} || { echo "[-] Error happen while delete ${device}"; exit 1; }
+else
+	echo "[-] Can't find disk with type Linux filesystem"
+	echo "[+] Removing loop device"
+	losetup -d ${loop} || { echo "[-] Error happen while delete ${device}"; exit 1; }
+	exit 1
+fi
+
