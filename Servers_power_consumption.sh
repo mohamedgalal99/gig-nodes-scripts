8a8c62ef1403a451f099fa080be1c7c7b07a4886
mohamedgalal99 <mohamedselim.jobs@gmail.com>
2019-07-09 12:32:15 +0200

    create log file

--- Servers_power_consumption.sh
+++ Servers_power_consumption.sh
@@ -22,7 +22,26 @@ function valid_ip()
 }
 
 
+
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ "${ip}" =~ [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} ]]
+    then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]] && stat=0
+    fi
+    return $stat
+}
+
+
 [[ -f "servers" ]] || { echo "[-] Can't find servers file"; exit 1; }
+[[ -f "server_power.log" ]] || touch server_power.log
 for i in $(cat servers)
 do
     server_ip="$(echo "${i}" | awk -F',' '{print $1}')"
@@ -32,6 +51,7 @@ do
     x=$?
     [[ $x != 0 ]] && { echo "[-] server ip is wronge formated: ${server_ip}"; continue; }
 
-    echo "[*] Server ${server_ip}"
-    ipmitool -I lanplus -H "${server_ip}" -U "${server_user}" -P ${server_password} sensor get Power1 Power2 | grep 'Sensor Reading' || echo "[-] Can't connect to Server"
+    echo "[*] Server ${server_ip}" | tee -a server_power.log
+    echo -ne "\t"
+    echo $(ipmitool -I lanplus -H "${server_ip}" -U "${server_user}" -P ${server_password} sensor get Power1 Power2 | grep 'Sensor Reading' || echo "[-] Can't connect to Server ${server_ip}") | tee -a server_power.log
 done
